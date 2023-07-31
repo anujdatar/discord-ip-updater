b015fb44bcc43fa099143489a417924ac7c9147a
Anuj Datar <anuj.datar@gmail.com>
2023-07-31 19:16:27 +0530

    better logging for container-setup step

--- scripts/container-setup.sh
+++ scripts/container-setup.sh
@@ -4,7 +4,6 @@ print_breaker() {
     echo "-----------------------------------------------"
 }
 
-echo "Container spun up at: [$(date)]"
 # #####################################################################
 # Step 1: set up timezone
 if [ -z "$TZ" ]; then
@@ -17,17 +16,19 @@ fi
 
 echo "Starting Discord-IP-Updater: [$(date)]"
 print_breaker
+# #####################################################################
+echo "Performing basic container parameter check..."
+# Step 2: Check discord webhook
 if [ -f "$WEBHOOK_FILE" ]; then
   WEBHOOK=$(cat $WEBHOOK_FILE)
 fi
 if [ -z "$WEBHOOK" ]; then
   echo "Please enter valid WEBHOOK env variable or WEBHOOK_FILE secret"
   exit 1
-else
-  echo "Webhook    --- OK"
 fi
-
-# 2. Record Type
+echo "Webhook    --- OK"
+# #####################################################################
+# Step 3: Record Type
 if [ "$RECORD_TYPE" == "A" ]; then
   echo "Record type to be updated: A (IPv4)"
 elif [ "$RECORD_TYPE" == "AAAA" ]; then
@@ -36,22 +37,18 @@ else
   RECORD_TYPE="A"
   echo "Unknown record type, assuming A-record (IPv4)"
 fi
-
-# 3. Message
+# #####################################################################
+# Step 4: Message
 if [ -z "$MESSAGE" ]; then
   MESSAGE="server ip update: "
 fi
-
 # #####################################################################
 # Step 2: Save to config file
-print_breaker
 touch /old_record_ip
-
 echo "WEBHOOK=\"$WEBHOOK\"" > /config.sh
 echo "RECORD_TYPE=\"$RECORD_TYPE\"" >> /config.sh
 echo "MESSAGE=\"$MESSAGE\"" >> /config.sh
 # #####################################################################
-
-echo "Discord webhook container setup complete"
 print_breaker
+echo "Discord webhook container setup complete"
 print_breaker
