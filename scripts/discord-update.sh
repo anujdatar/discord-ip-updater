36c0389c8e209fde4c8e257d20557af34fbd5eeb
Anuj Datar <anuj.datar@gmail.com>
2023-08-01 11:53:48 +0530

    using if else in old-ip check and ddns update, exit 0 causing container restart
    fixed some indentation

--- scripts/discord-update.sh
+++ scripts/discord-update.sh
@@ -32,9 +32,9 @@ get_ip6() {
 # #####################################################################
 # Step 1: Get the current IP address
 if [ "$RECORD_TYPE" == "A" ]; then
-	CURRENT_IP=$(get_ip4)
+  CURRENT_IP=$(get_ip4)
 elif [ "$RECORD_TYPE" == "AAAA" ]; then
-	CURRENT_IP=$(get_ip6)
+  CURRENT_IP=$(get_ip6)
 fi
 
 if [ -z $CURRENT_IP ]; then
@@ -46,18 +46,18 @@ fi
 OLD_IP=$(cat /old_record_ip)
 if [ "$OLD_IP" == "$CURRENT_IP" ]; then
   echo "[$(date)]: IP unchanged, not updating.  IP: $CURRENT_IP"
-  exit 0
-fi
 # #####################################################################
 # Step 3: send updated ip to discord
-curl -s --header "Content-Type:application/json" \
-  --request POST \
-  --data "{\"content\": \"$MESSAGE $CURRENT_IP\"}" \
-  $WEBHOOK
-
-if [ $? -ne 0 ]; then
-  echo "[$(date)]: Discord message failed. Curr IP: $CURRENT_IP"
 else
-  echo "[$(date)]: Discord message successful.. IP: $CURRENT_IP"
-  echo $CURRENT_IP > /old_record_ip
+  curl -s --header "Content-Type:application/json" \
+    --request POST \
+    --data "{\"content\": \"$MESSAGE $CURRENT_IP\"}" \
+    $WEBHOOK
+
+  if [ $? -ne 0 ]; then
+    echo "[$(date)]: Discord message failed. Curr IP: $CURRENT_IP"
+  else
+    echo "[$(date)]: Discord message successful.. IP: $CURRENT_IP"
+    echo $CURRENT_IP > /old_record_ip
+  fi
 fi
